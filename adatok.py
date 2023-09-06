d0fe6808b138176a4bc2d417b10024faab24fbdd
boritgy <55995915+boritgy@users.noreply.github.com>
2023-09-06 22:05:10 +0200

    Update adatok.py

--- adatok.py
+++ adatok.py
@@ -157,10 +157,10 @@ def ep(eredmeny_url, maz, taz):
     soup = BeautifulSoup(req.content)
     pages = int(soup.find('small', attrs = {'class':'search-results'}).text.split(' / ')[1]) // 20 + 1
     for page in range(1, pages+1):
-      eu_url = "https://www.valasztas.hu/szavazokorok_ep2019?p_p_id=epszavazokorok_WAR_nvinvrportlet&p_p_lifecycle=1&p_p_state=maximized&p_p_mode=view&_epszavazokorok_WAR_nvinvrportlet_searchSortColumn=SORSZAM&_epszavazokorok_WAR_nvinvrportlet_megyeKod2="+maz+"&_epszavazokorok_WAR_nvinvrportlet_vlId=291&_epszavazokorok_WAR_nvinvrportlet_searchSortType=asc&_epszavazokorok_WAR_nvinvrportlet_vltId=684&_epszavazokorok_WAR_nvinvrportlet_telepulesKod2="+taz+"&_epszavazokorok_WAR_nvinvrportlet_valasztasTipusKod=E&_epszavazokorok_WAR_nvinvrportlet_delta=20&_epszavazokorok_WAR_nvinvrportlet_resetCur=false&_epszavazokorok_WAR_nvinvrportlet_cur=%s#_epszavazokorok_WAR_nvinvrportlet_paginator"
-      req = requests.get(eu_url %str(page))
-      req = s.mount(eu_url %str(page), AddedCipherAdapter())
-      req = s.get(eu_url %str(page))
+      eu_url = "https://www.valasztas.hu/szavazokorok_ep2019?p_p_id=epszavazokorok_WAR_nvinvrportlet&p_p_lifecycle=1&p_p_state=maximized&p_p_mode=view&_epszavazokorok_WAR_nvinvrportlet_searchSortColumn=SORSZAM&_epszavazokorok_WAR_nvinvrportlet_megyeKod2="+maz+"&_epszavazokorok_WAR_nvinvrportlet_vlId=291&_epszavazokorok_WAR_nvinvrportlet_searchSortType=asc&_epszavazokorok_WAR_nvinvrportlet_vltId=684&_epszavazokorok_WAR_nvinvrportlet_telepulesKod2="+taz+"&_epszavazokorok_WAR_nvinvrportlet_valasztasTipusKod=E&_epszavazokorok_WAR_nvinvrportlet_delta=20&_epszavazokorok_WAR_nvinvrportlet_resetCur=false&_epszavazokorok_WAR_nvinvrportlet_cur="+page+"#_epszavazokorok_WAR_nvinvrportlet_paginator"
+      req = requests.get(eu_url)
+      req = s.mount(eu_url, AddedCipherAdapter())
+      req = s.get(eu_url)
       soup = BeautifulSoup(req.content)
       link_divs = soup.find_all('div', attrs = {"class" : "nvi-search-container-row"})
       links = []
